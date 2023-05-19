d1d5bf706b3ac7c25b67ef084267319cb3e42004
jeffersonnjohan <jeffersonnjohan@gmail.com>
2023-05-19 22:05:06 +0700

    c

---
+++ resources/views/profile/profileMahasiswa.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Profile Mahasiswa</title>
+</head>
+<body>
+    <h1>Jefferson Johan</h1>
+    <p>NIM: 2502041224</p>
+    <p>Cawu: 5</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Route::get('/', function () {
 
 Route::get('/mahasiswa', function(){
     return view('profile.profileMahasiswa');
-})
+});
